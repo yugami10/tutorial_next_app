a23649e49da62d70b264c3053fdb92dd95ef1cc1
yugami10 <yugami333@gmail.com>
2026-02-14 14:57:31 +0900

    プロフィールの簡易ページ作成

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,29 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Image src="/profile/banana.jpeg" alt="プロフィール画像" width={100} height={100} />
-      <p>Hello</p>
+    <div className="content-center h-screen">
+      <div className="flex justify-center gap-x-10">
+        <div>
+          <Image
+            src="/profile/banana.jpeg"
+            alt="プロフィール画像"
+            width={200}
+            height={200}
+            objectFit="contain"
+          />
+        </div>
+        <div>
+          <p>なまえ</p>
+          <p>自己紹介</p>
+          <p>
+            ああああああああ。
+            <br />
+            いいいいいいいいいいい。
+            <br />
+            よろしくお願いします。
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
